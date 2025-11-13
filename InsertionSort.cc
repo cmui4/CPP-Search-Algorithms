bab22e7dfdee25d4329ac94342a8b77dda1e23e0
rlavel01 <rlavel01@villanova.edu>
2025-11-12 22:19:53 -0500

    changes made to insertion sort and working through errors, changes input file to 3 elements for testing.

--- InsertionSort.cc
+++ InsertionSort.cc
@@ -6,9 +6,26 @@
  */
 
 #include "myheaders.h"
+#include <iostream>
 
 void InsertionSort(long arr[], int size){
 
+	//sorts through each element of the array starting at the 1st element
+	for(int i = 1, j ; i < size; i++){
+		//stores the current element that is waiting to be put in array
+		int tmp = arr[i];
+		cout << "outer loop iteration: " << i << " : " << tmp << endl;
+
+		//shifts the already sorted element to the right, does this until the appropriate position of tmp is found
+		for(j = i; j > 0 && tmp < arr[j-1]; j--){
+
+			arr[j] = arr[j - 1];
+		}
+
+		//puts tmp into the correct position of the array after being sorted
+		cout << "inner loop iteration: " << j << " : " << arr[j] << endl;
+		arr[j] = tmp;
+	}
 }
 
 
--- lab5.cc
+++ lab5.cc
@@ -55,7 +55,7 @@ int main() {
 			
 					// Call your Sort function A here to sort the array 'wData''
 			        //  Note that 'wData' is of size 'sz' (see case 1).
-
+					InsertionSort(wData, sz);
 
 			
 					end_time = clock();		// end cpu timer
